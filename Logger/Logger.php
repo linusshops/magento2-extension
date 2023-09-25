0d6c010341da90da17d4a1b44c2c9539dc71e350
Matt Skelton <waterloomatt@gmail.com>
2023-09-25 15:20:29 -0400

    Update Logger class to match \Monolog\Logger

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -11,6 +11,7 @@ namespace Bazaarvoice\Connector\Logger;
 use Bazaarvoice\Connector\Api\ConfigProviderInterface;
 use Exception;
 use Magento\Framework\App\State;
+use Monolog\DateTimeImmutable;
 
 /**
  * Class Logger
@@ -76,7 +77,7 @@ class Logger extends \Monolog\Logger
      *
      * @return bool
      */
-    public function addRecord($level, $message, array $context = [])
+    public function addRecord($level, $message, array $context = [], ?DateTimeImmutable $datetime = NULL): bool
     {
         if (is_array($message)) {
             $message = print_r($message, $return = true);
